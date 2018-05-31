4cbab3c9c20e4fda25fdf73e657e288e8f472cb9
Tom Vander Aa <vanderaa@imec.be>
2018-05-31 15:17:29 +0200

    solve block cg test work for sparse

--- lib/smurff-cpp/Tests/TestsLinop.cpp
+++ lib/smurff-cpp/Tests/TestsLinop.cpp
@@ -139,12 +139,16 @@ TEST_CASE( "SparseMatrix/solve_blockcg", "BlockCG solver (1rhs)" )
    }
    REQUIRE( niter <= 4);
 }
+#endif
 
 TEST_CASE( "SparseFeat/solve_blockcg_1_0", "BlockCG solver (3rhs separately)" ) 
 {
-   int rows[9] = { 0, 3, 3, 2, 5, 4, 1, 2, 4 };
-   int cols[9] = { 1, 0, 2, 1, 3, 0, 1, 3, 2 };
-   SparseFeat sf(6, 4, 9, rows, cols);
+   std::vector<uint32_t> rows = { 0, 3, 3, 2, 5, 4, 1, 2, 4 };
+   std::vector<uint32_t> cols = { 1, 0, 2, 1, 3, 0, 1, 3, 2 };
+   MatrixConfig sf(6, 4, rows, cols);
+   Eigen::SparseMatrix<double> F = matrix_utils::sparse_to_eigen(sf);
+   Eigen::SparseMatrix<double> Ft = F.transpose();
+
    Eigen::MatrixXd B(3, 4), X(3, 4), X_true(3, 4);
  
    B << 0.56,  0.55,  0.3 , -1.78,
@@ -154,60 +158,18 @@ TEST_CASE( "SparseFeat/solve_blockcg_1_0", "BlockCG solver (3rhs separately)" )
    X_true << 0.35555556,  0.40709677, -0.16444444, -0.87483871,
              1.69333333, -0.12709677, -1.94666667,  0.49483871,
              0.66      , -0.04064516, -0.78      ,  0.65225806;
+
+   auto op = [&F, &Ft](const Eigen::MatrixXd &X) -> Eigen::MatrixXd
+   {
+      return (Ft * (F * X.transpose())).transpose() + 0.5 * X;
+   };
  
-   smurff::linop::solve_blockcg(X, sf, 0.5, B, 1e-6, 1000, 1, 0);
+   smurff::linop::solve_blockcg(X, op, B, 1e-6, 1000, 1, 0);
    for (int i = 0; i < X.rows(); i++) {
      for (int j = 0; j < X.cols(); j++) {
        REQUIRE( X(i,j) == Approx(X_true(i,j)) );
      }
    }
-}
-#endif
-
-#if 0
-TEST_CASE( "MatrixXd/compute_uhat", "compute_uhat for MatrixXd" ) {
-   Eigen::MatrixXd beta(2, 4), feat(6, 4), uhat(2, 6), uhat_true(2, 6);
-   beta << 0.56,  0.55,  0.3 , -1.78,
-           1.63, -0.71,  0.8 , -0.28;
-   feat <<  -0.83,  -0.26,  -0.52,  -0.27,
-             0.91,  -0.48,   0.50,  -0.20,
-            -0.59,   1.94,  -1.09,   0.86,
-            -0.08,   0.62,  -1.10,   0.96,
-             1.44,   0.89,  -0.45,   0.2,
-            -1.33,  -1.42,   0.03,  -2.32;
-   uhat_true <<  -0.2832,  0.7516,  -1.1212,  -1.7426,  0.8049,   2.6128,
-                 -1.5087,  2.2801,  -3.4519,  -1.7194,  1.2993,  -0.4861;
-   smurff::linop::compute_uhat(uhat, feat, beta);
-   for (int i = 0; i < uhat.rows(); i++) {
-     for (int j = 0; j < uhat.cols(); j++) {
-       REQUIRE( uhat(i,j) == Approx(uhat_true(i,j)) );
-     }
-   }
-}
-#endif
-
-TEST_CASE( "linop/solve_blockcg_dense/fail", "BlockCG solver for dense (3rhs separately)" ) 
-{
-   int rows[9] = { 0, 3, 3, 2, 5, 4, 1, 2, 4 };
-   int cols[9] = { 1, 0, 2, 1, 3, 0, 1, 3, 2 };
-   Eigen::MatrixXd B(3, 4), X(3, 4), X_true(3, 4), sf(6, 4);
- 
-    sf = Eigen::MatrixXd::Zero(6, 4);
-    for (int i = 0; i < 9; i++) {
-       sf(rows[i], cols[i]) = 1.0;
-    }
- 
-   B << 0.56,  0.55,  0.3 , -1.78,
-        0.34,  0.05, -1.48,  1.11,
-        0.09,  0.51, -0.63,  1.59;
- 
-   auto op = [&sf](const Eigen::MatrixXd &X) -> Eigen::MatrixXd
-   {
-      return (sf.transpose() * (sf * X.transpose())).transpose() + 0.5 * X;
-   };
-
-   // this system is unsolvable
-   REQUIRE_THROWS(smurff::linop::solve_blockcg(X, op, B, 1e-6, 1000, true));
 }
 
 TEST_CASE( "linop/solve_blockcg_dense/ok", "BlockCG solver for dense (3rhs separately)" ) 
